b58ca0cbabc47eb676b2b7485b7f39c71ed47a10
ShaddyDC <shaddythefirst@gmail.com>
2022-06-16 13:50:56 +0200

    Add note on future updates

--- src/cloth_management.rs
+++ src/cloth_management.rs
@@ -107,6 +107,30 @@ pub(crate) async fn get_clothes(
 ) -> Result<Json<Vec<ClothOut>>, ErrorResponse> {
     use schema::clothes::dsl::*;
 
+    // Switch when diesel 2 is supported in Rocket
+    // https://github.com/SergioBenitez/Rocket/issues/2209
+    // Also use opportunity to use .first() in diesel instead of vector
+
+    // use crate::schema::*;
+    // use diesel::dsl::count;
+    //
+    // let cloth_list = conn
+    //     .run(move |c| {
+    //         clothes::table
+    //             .filter(clothes::user_id.eq(user.id))
+    //             .left_join(wears::table)
+    //             .group_by(clothes::id)
+    //             .select((
+    //                 (clothes::id, clothes::user_id, clothes::cloth_name),
+    //                 count(wears::id),
+    //             ))
+    //             .load::<(Cloth, i64)>(c)
+    //             .map_err(|_| {
+    //                 ErrorResponse::new(Status { code: 500 }, "Couldn't load clothes".to_string())
+    //             })
+    //     })
+    //     .await;
+
     let cloth_list = conn
         .run(move |c| {
             clothes
